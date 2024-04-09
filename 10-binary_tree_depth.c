4dcd35055c6c3980a399a05426c99cc359ecc2bc
ngwenyatm <thabsnspaces@gmail.com>
2024-04-09 09:18:12 -0700

    binary trees

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,15 +8,13 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return (0);
-    }
-
-    if (tree->parent)
-    {
-        return (binary_tree_depth(tree->parent) + 1);
-    }
-
-    return (0);
+	if (!tree)
+	{
+		return (0);
+	}
+	if (tree->parent)
+	{
+		return (binary_tree_depth(tree->parent) + 1);
+	}
+	return (0);
 }
