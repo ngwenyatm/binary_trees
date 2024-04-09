b6e9e1ce3900945379adc6702d64d3360abdece0
ngwenyatm <thabsnspaces@gmail.com>
2024-04-09 15:16:26 -0700

    binary trees

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,18 +8,19 @@
  * otherwise pointer to sibling
  */
 
-binary_tree_t *binary_tree_sibling(binary_tree_t *node) {
-  if ((node == NULL) || (node->parent == NULL))
-  {
-	  return NULL;
-  }
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if ((node == NULL) || (node->parent == NULL))
+	{
+		return (NULL);
+	}
 
-  if (node->parent->left == node)
-  {
-	  return node->parent->right;
-  }
-  else
-  {
-	  return node->parent->left;
-  }
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	else
+	{
+		return (node->parent->left);
+	}
 }
