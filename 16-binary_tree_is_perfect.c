c9e953a154c303c80c202a73c14cf6df4d69982a
MakamuT <makamuthe2@gmail.com>
2024-04-10 06:35:51 -0700

    binary trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,15 +6,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height, right_height;
+	size_t left_height, right_height;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-        return (left_height > right_height ? left_height + 1 : right_height + 1);
+	return (left_height > right_height ? left_height + 1
+			: right_height + 1);
 }
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
