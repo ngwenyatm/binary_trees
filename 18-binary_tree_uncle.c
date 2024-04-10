1dfb507cd884666a0622133bae38992a53a73d25
MakamuT <makamuthe2@gmail.com>
2024-04-10 12:37:28 -0700

    binary trees

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,44 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ * Return: NULL If node is NULL or the parent is NULL or no sibling,
+ * otherwise pointer to sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if ((node == NULL) || (node->parent == NULL))
+	{
+		return (NULL);
+	}
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	else
+	{
+		return (node->parent->left);
+	}
+}
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height > right_height ?
+			left_height + 1 : right_height + 1);
+}
+/**
  * binary_tree_uncle -  finds the uncle of a node
  * @node: pointer to the node to find the uncle
  * Return: NULL or 0
