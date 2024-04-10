a015874a3ab580559716023c7f5c3dfa399ef851
MakamuT <makamuthe2@gmail.com>
2024-04-10 06:29:15 -0700

    binary trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,14 +17,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
         return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
- * Return: NULL
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 0 or NULL
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	return (binary_tree_height(tree->left) -
-			binary_tree_height(tree->right));
+	size_t nodes = binary_tree_size(tree);
+	size_t height = binary_tree_height(tree);
+
+	size_t perfect_nodes = (1 << height) - 1;
+
+	return (nodes == perfect_nodes ? 1 : 0);
 }
--- binary_tree_height.c
+++ binary_tree_height.c
@@ -1,4 +1,3 @@
-#include "binary_trees.h"
 /**
  * binary_tree_height - measure the height of a binary tree
  * @tree: pointer
@@ -15,16 +14,4 @@ size_t binary_tree_height(const binary_tree_t *tree)
         right_height = binary_tree_height(tree->right);
 
         return (left_height > right_height ? left_height + 1 : right_height + 1);
-}
-/**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
- * Return: NULL
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (binary_tree_height(tree->left) -
-			binary_tree_height(tree->right));
 }
