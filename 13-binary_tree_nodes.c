11c90a599d32eaf2cb36c067d77767bf45ac009a
ngwenyatm <thabsnspaces@gmail.com>
2024-04-10 05:47:46 -0700

    binary trees

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,7 +15,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	{
 		if (tree->left || tree->right)
 		{
-			nodes + 1;
+			nodes = nodes + 1;
 		}
 
 		nodes = nodes + binary_tree_nodes(tree->left);
