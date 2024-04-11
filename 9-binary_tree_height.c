2fcd47d6e65eeee8ab534667158ac940dcbff61d
MakamuT <makamuthe2@gmail.com>
2024-04-11 13:31:24 -0700

    binary trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,24 +9,21 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  if (tree == NULL) 
-  {
-    return 0;
-  }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-  size_t l_height;
-  size_t r_height;
+	size_t l_height;
+	size_t r_height;
 
-  l_height = binary_tree_height(tree->left);
-  r_height = binary_tree_height(tree->right);
-
-  if (l_height > r_height)
-  {
-    return l_height + 1;
-  }
-  else
-  {
-    return r_height + 1;
-  }
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
+	if (l_height > r_height)
+	{
+		return (l_height + 1);
+	}
+	else
+		return (r_height + 1);
 }
 
