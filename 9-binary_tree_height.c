f2699d6bbbd1991114a231c639bbbbf6bf11ff99
ngwenyatm <thabsnspaces@gmail.com>
2024-04-11 14:56:41 -0700

    binary trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,13 +11,19 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t l_height = 0;
-		size_t r_height = 0;
 
-		l_height = tree->left ? (binary_tree_height(tree->left) + 1) : 0;
-		r_height = tree->right ? (binary_tree_height(tree->right) + 1) : 0;
-		return ((l_height > r_heighht) ? l_height : r_height);
-	}
-	return (0);
+	size_t l_height = 0;
+	size_t r_height = 0;
+
+
+  l_height = binary_tree_height(tree->left);
+
+  r_height = binary_tree_height(tree->right);
+
+  if (l_height > r_height) {
+    return l_height + 1;
+  } else {
+    return r_height + 1;
+  }
 }
-
+}
