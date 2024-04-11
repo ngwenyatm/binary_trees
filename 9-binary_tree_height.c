79f40ade75af13390573656f9125212c662ce1cd
ngwenyatm <thabsnspaces@gmail.com>
2024-04-11 06:59:37 -0700

    binary trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,18 +8,16 @@
  */
 
 size_t binary_tree_height(const binary_tree_t *tree) {
-  // Check if the tree is empty (NULL pointer)
+ 
   if (!tree) {
     return 0;
   }
 
-  // Calculate height of left subtree recursively
+
   size_t left_height = binary_tree_height(tree->left);
 
-  // Calculate height of right subtree recursively
   size_t right_height = binary_tree_height(tree->right);
 
-  // Determine the maximum height
   if (left_height > right_height) {
     return left_height + 1;
   } else {
