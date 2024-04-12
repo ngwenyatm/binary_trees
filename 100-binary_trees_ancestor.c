5e622a5e7de3662a99a875aad9cd0cd28bb1a57a
ngwenyatm <thabsnspaces@gmail.com>
2024-04-12 05:51:05 -0700

    binary trees

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,26 +9,27 @@
  */
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                      const binary_tree_t *second)
+		const binary_tree_t *second)
 {
+	const binary_tree_t *anc1 = first->parent;
+	const binary_tree_t *anc2 = second->parent;
 
-  const binary_tree_t *Ancestor1 = first->parent;
-  const binary_tree_t *Ancestor2 = second->parent;
+	if (!first || !second)
+	{
+		return (NULL);
+	}
 
-  if (!first || !second) {
-    return (NULL);
-  }
-  if (first == second) {
-    return ((binary_tree_t *)first);
-  }
-
-  if (first == Ancestor2 || !Ancestor1 || (!Ancestor1->parent && Ancestor2)) {
-    return (binary_trees_ancestor(first, Ancestor2));
-  }
-
-  else if (Ancestor1 == second || !Ancestor2 || (!Ancestor2->parent && Ancestor1)) {
-    return (binary_trees_ancestor(Ancestor1, second));
-  }
-
-  return (binary_trees_ancestor(Ancestor1, Ancestor2));
+	if (first == second)
+	{
+		return ((binary_tree_t *)first);
+	}
+	if (first == anc2 || !anc1 || (!anc1->parent && anc2))
+	{
+		return (binary_trees_ancestor(first, ancestor2));
+	}
+	else if (anc1 == second || !anc2 || (!anc2->parent && anc1))
+	{
+		return (binary_trees_ancestor(anc1, second));
+	}
+	return (binary_trees_ancestor(anc1, anc2));
 }
